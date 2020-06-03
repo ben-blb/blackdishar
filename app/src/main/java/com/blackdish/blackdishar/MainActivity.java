9aad80258fe5671bfb037e154bf695dcec2261fb
ben-blb <ben.blb@live.com.mx>
2020-06-02 22:25:07 -0500

    testing code

--- app/src/main/java/com/blackdish/blackdishar/MainActivity.java
+++ app/src/main/java/com/blackdish/blackdishar/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.blackdish.blackdishar.controllers.ArController;
+import com.blackdish.blackdishar.models.ArPlateModel;
+import com.google.ar.sceneform.ux.ArFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArFragment arFragment;
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        ArController arController = new ArController(arFragment);
+        ArPlateModel arPlateModel = new ArPlateModel();
+        arPlateModel.setImagenPlatillo("https://blackdish.mx/blackdish-beta-v3/backend/model/img_admin/IMG-5477-removebg-preview.png"); //por ahora usamos este como placeholder para hacer las pruebas del arController
+        arController.setPlateModel(arPlateModel);
+        arController.draw();
     }
 }
--- app/src/main/java/com/blackdish/blackdishar/controllers/ArController.java
+++ app/src/main/java/com/blackdish/blackdishar/controllers/ArController.java
@@ -1,8 +1,19 @@
 package com.blackdish.blackdishar.controllers;
 
 import com.blackdish.blackdishar.models.ArPlateModel;
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class ArController {
     private ArPlateModel plateModel;
+    private ArFragment arFragment;
+    public ArController(ArFragment viewer){
+        arFragment = viewer;
+    }
 
+    public void setPlateModel(ArPlateModel plateModel) {
+        this.plateModel = plateModel;
+    }
+
+    public void draw() {
+    }
 }
